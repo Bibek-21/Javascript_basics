e15bb6cb4b193c7143e1cdfe833b8c1cbd63cf88
Bibek Regmi <bibek.regmi@swifttech.com>
2023-06-01 15:21:34 +0545

    chiya break

--- array/array.js
+++ array/array.js
@@ -34,11 +34,31 @@ const filterfnc = (element)=> element.length<1;     // it is used to filter elem
  const findvalue = (element)=> element.length>2;     //returns the first element in the provided array that satisfies the given condition
 // console.log(alpha.find(findvalue));
 // console.log(alpha.findIndex(findvalue));        // it returns the index of the first element that passes the test function
-console.log(alpha.findLast(findvalue));     // it reverses the value and returns value of first index
-console.log(alpha.findLastIndex(findvalue));        // it returns the index for above found value
+// console.log(alpha.findLast(findvalue));     // it reverses the value and returns value of first index
+// console.log(alpha.findLastIndex(findvalue));        // it returns the index for above found value
 
 const array6 = [1,2,3,1,5,7];
-console.log(array6.flat(2));        // it flats the array within an array  output: [(6) [1, 2, 3, 1, 5, 7]]
+// console.log(array6.flat(2));        // it flats the array within an array  output: [(6) [1, 2, 3, 1, 5, 7]]
 
-console.log(array6.map(map_fnc));
-console.log(array6.flatMap(map_fnc));
+// console.log(array6.map(map_fnc));
+// console.log(array6.flatMap(map_fnc));       //i did not understand the dif between this in details
+
+const y = element => console.log(element);
+// array6.forEach(y);      //it executes the provided function for each element in the array
+
+// from creates  a new copy of array from array object
+// console.log(Array.from('foooo'));
+// syntax Array.from(arrayLike, mapFn)
+// console.log(Array.from([1,2,4],x=>x*2));
+
+// console.log(Array.fromAsync('asdf')) //it gave me error
+
+// console.log(array6.includes(5));        //check if it is present or not
+
+// console.log(array6.indexOf(3));  //gives index of the element present in the array
+// console.log(array6.indexOf(123));           // gives -1 as output
+// console.log(Array.isArray(array6));     //checks if the provided arg is array or  not
+
+console.log(array6.join('-'));
+console.log(array6.join('+'));
+console.log(array6.join('*'));
