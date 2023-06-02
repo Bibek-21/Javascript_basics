fa0b997a686b816cdc97d8c7dc9bb2bba268d859
Bibek Regmi <bibek.regmi@swifttech.com>
2023-06-02 15:13:58 +0545

    brach bata push try

--- function/arrow_function.js
+++ function/arrow_function.js
@@ -1,6 +1,13 @@
 const test = ()=>
 {
-    return console.log(`Testing worldf`);
+    // return console.log(`Testing worldf`);
 }
 
 console.log(test());
+const ax1 = (a,b)=>
+{
+    let compt = a+b/b;
+    return compt;
+
+}
+console.log(ax1(2,4));
