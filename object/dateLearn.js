7da84f8d54f3c0b761488785013793ab2af9852e
Bibek Regmi <bibek.regmi@swifttech.com>
2023-06-04 16:56:15 +0545

    dateLearn file updated

---
+++ object/dateLearn.js
@@ -0,0 +1,15 @@
+
+// Syntax:  new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds)
+const date1 = new Date();
+console.log(date1);
+// const date2 = new Date('1970-02-02T21:54:00.000Z');
+// console.log(date2.getDay());
+const date3 =   new Date('2023/06/04 12:21:17:99');
+// console.log(date3);
+// console.log(date3.getDay());            //sunday ie 0
+// console.log(date3.getFullYear());       //2023
+// console.log(date3.getHours());          //12                       <--- Outputs
+// console.log(date3.getMinutes());        //21
+// console.log(date3.getMonth());          //5
+// console.log(date3.getSeconds());        //17
+// console.log(date3.getMilliseconds());   //99
