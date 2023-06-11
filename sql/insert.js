ce764a68623225cebc7f98157de54f787a7ef3b4
Bibek Regmi <bibek.regmi@swifttech.com>
2023-06-11 17:04:59 +0545

    aaja xitto janxu yr ma pani

--- sql/insert.js
+++
@@ -1,5 +0,0 @@
-// let mysql = require("sql.js");
-import Connection from "mysql2/typings/mysql/lib/Connection";
-import "sql.js";
-let quer =`insert into marksheet()`
-Connection.query(quer)
--- sql/sql.js
+++ sql/sql.js
@@ -23,30 +23,65 @@ connection.connect(function (err) {
     // let sql = `delete from  marksheet where ID=6`;
     // let sql = `delete from marksheet where ID = 7 or ID = 8 `
     // let sql =`drop table marksheet`;  //deletes all the table
-//     let sql = `update marksheet 
-// set Name= 'Rajan bhadel',Maths=78,Science= 87,Social= 93,English= 95,Nepali= 80
-// where ID=5;
-//   `
-// let sql =  `select * from marksheet         
-// limit 2`                        // katiota rows chaiyo limit willsay 
+    //     let sql = `update marksheet 
+    // set Name= 'Rajan bhadel',Maths=78,Science= 87,Social= 93,English= 95,Nepali= 80
+    // where ID=5;
+    //   `
+    // let sql =  `select * from marksheet         
+    // limit 2`                        // katiota rows chaiyo limit willsay 
 
-// let sql = `select * from marksheet limit 3 offset 2`;       //the limit 3 starts after 2 rows
-// let sql = `delete from user where id=6;`;
-// let sql = `select * from user`;
-// let sql  = `delete from user where id = 3; `;
+    // let sql = `select * from marksheet limit 3 offset 2`;       //the limit 3 starts after 2 rows
+    // let sql = `delete from user where id=6;`;
+    // let sql = `select * from user`;
+    // let sql  = `delete from user where id = 3; `;
 
-// let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
-// from user
-// join marksheet on 
-// user.name=marksheet.Name;`;          //join and innner join nare same
-// let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
-// from user
-// left join marksheet on 
-// user.name=marksheet.Name;`;      //all the names of the user will be displayed though their values may be null
-let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
-from user
-right join marksheet on 
-user.name=marksheet.Name;`;         //it gives value for all 6 students from marksheet though they may not be present in user
+    // let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
+    // from user
+    // join marksheet on 
+    // user.name=marksheet.Name;`;          //join and innner join nare same
+    // let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
+    // from user
+    // left join marksheet on 
+    // user.name=marksheet.Name;`;      //all the names of the user will be displayed though their values may be null
+    // let sql = `select user.name as Name, marksheet.ID as RollNo,marksheet.Maths as MathsMarks
+    // from user
+    // right join marksheet on 
+    // user.name=marksheet.Name;`;         //it gives value for all 6 students from marksheet though they may not be present in user
+    // let sql = `create table studentsInfo(
+    //     RollNO int not null auto_increment,
+    //     Students_Name varchar(255),
+    //     Address varchar(255),
+    //     Remarks varchar(255),
+    //     primary key(RollNO)
+    //     );`;
+    // let sql = `select * from studentsinfo`;
+
+    // let sql = `insert into studentsinfo(Students_Name,Address)
+    // values('Aaditya Khanal','swyambhu'),
+    // ('Aayush Joshi','Satdobato'),
+    // ('Bibek Regmi','ktm'),
+    // ('Bibhushan Poudel','bkt'),
+    // ('Rojan Dahal','brtnagar')`
+    // let sql =   `delete from studentsinfo
+    // where RollNo is null`;
+
+    // let sql = `alter table studentsinfo
+    // add Admission_date date`;
+
+    // let sql = `update studentsinfo
+    // set Admission_date='2080-01-01'
+    // where Students_Name= 'Aaditya Khanal' and RollNo ='1'`;
+    let sql =`select * from studentsinfo`;
+//     let sql = `update studentsinfo
+//  set Admission_date='2080-01-02'
+// where Students_Name= 'Aayush Joshi' and RollNo ='2'`;
+// let sql = `update studentsinfo
+//  set Admission_date='2080-01-09'
+//  where Students_Name= 'Bibek Regmi' and RollNo ='3'`;
+//  let sql = `update studentsinfo
+//  set Admission_date='2080-01-11'
+//  where Students_Name= 'Bibhushan Poudel' and RollNo ='4'`
+ 
     connection.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
